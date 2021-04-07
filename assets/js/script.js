0a86233329ff10e59fe52e75bfd74cf51ec2ff69
stephje <s.jenkins3018@gmail.com>
2021-04-07 15:49:23 +0000

    Add time features
    Colour change for present and future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
 //display current date
-var date = moment();
-jQuery('#currentDay').text(date.format('dddd Do MMMM, YYYY'));
+var now = moment();
+$('#currentDay').text(now.format('dddd Do MMMM, YYYY HH:mm'));
+
+// var currentTime = now.format('H')
+var currentTime = 9;
+
+$(document).ready(function(){
+
+    for (let i = 9; i <= 17; i++) {
+        if (i === currentTime) {
+            $(`#${i}-input`).css("background-color","red");
+        } else if (i > currentTime) {
+            $(`#${i}-input`).css("background-color","green");
+        } 
+    }
+
+});
+
+$("button").click(function() {
+    var userEntry = $(this).siblings(".user-entry").val();
+    console.log(userEntry);
+});
+

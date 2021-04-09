855f1f68ec15d60e943c5eeb0adc8e44f002e57f
stephje <s.jenkins3018@gmail.com>
2021-04-09 04:10:51 +0000

    Fix bug whereby colours did not update in real time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,33 @@
-//display current date
 var now = moment();
 
+//function that is invoked when the DOM is safe to manipulate
+$(document).ready(function(){
+
+    updateTimeSensitiveFeatures();
+    displaySchedule();
+
+});
+
+//function to invoke every 1000ms (1s)
+setInterval(updateTimeSensitiveFeatures, 1000)
+
+//Updates date/time and page colours
+function updateTimeSensitiveFeatures() {
+    displayDateAndTime();
+    setColours();
+}
+
+//display current date and time
 function displayDateAndTime(){
     now = moment();
     $('#currentDay').text(now.format('dddd Do MMMM, YYYY HH:mm a'));
 }
 
-//update time every 1000ms (1s)
-setInterval(displayDateAndTime, 1000)
-
 //now.format('H') is a string. Use parseInt to convert to a number
 var currentTime = parseInt(now.format('H'));
 
-//function to run only when the DOM is safe to manipulate
-$(document).ready(function(){
-
-    displayDateAndTime();
-    displaySchedule();
-
-    //set colour to pink for the current timeslot and green for timeslots in the future
+//set colour to pink for the current timeslot and green for timeslots in the future
+function setColours() {
     for (let i = 9; i <= 17; i++) {
         if (i === currentTime) {
             $('#' + i).css("background-color", "#eaaec1");
@@ -28,39 +37,7 @@ $(document).ready(function(){
             $('#' + i).css("background-color", "#f3f5f7");
         }
     }
-
-});
-
-//create a new array of length 9
-//if you create an empty array without specifying the length, splice doesn't work properly
-var schedule = new Array(9);
-
-//when a save button is clicked, append
-$("button").click(function() {
-
-    getSchedule();
-
-    //assign the value of the clicked button's silbing textarea element (identified by class) to a variable
-    var userEntry = $(this).siblings(".user-entry").val();
-
-    //assign the id of the button's sibling textarea element to a variable
-    var idOfUserEntry = $(this).siblings(".user-entry").attr("id");
-
-    //store both variables in an object
-    //idOfUserEntry is a string, so use parseInt to convert it to a number
-    var scheduleItem = {
-        time: parseInt(idOfUserEntry),
-        entry: userEntry
-    }
-
-    //splice the created into the array called schedule at the specific index
-    // index should be the value of "time" (which represents which hour block the content is from) minus 9, such that index 0 = 9AM content and so on
-    schedule.splice((scheduleItem.time - 9), 1, scheduleItem);
-    
-    //convert array into string and store in local storage
-    localStorage.setItem("schedule", JSON.stringify(schedule));
-
-});
+}
 
 //If there is a schedule in local storage, get it and parse it into a javascript object
 function getSchedule() {
@@ -83,4 +60,32 @@ function displaySchedule() {
     }
 }
 
+//create a new array of length 9. If an empty array is created without specifying the length, the splice method will not work as desired
+var schedule = new Array(9);
+
+//Function to execute when a save button is clicked - creates new object and splices into schedule array
+$("button").click(function() {
+
+    getSchedule();
+
+    //assign content of the clicked button's sibling textarea to a variable
+    var userEntry = $(this).siblings(".user-entry").val();
+
+    //assign ID of the clicked button's sibling textarea to a variable
+    var idOfUserEntry = $(this).siblings(".user-entry").attr("id");
+
+    //store both variables in an object. idOfUserEntry is a string, so use parseInt to convert it to a number
+    var scheduleItem = {
+        time: parseInt(idOfUserEntry),
+        entry: userEntry
+    }
+
+    //splice the created object into the "schedule" array at a specific index
+    //index should be the value of "time" (which represents which hour block the content is from) minus 9, such that index 0 = 9AM content and so on
+    schedule.splice((scheduleItem.time - 9), 1, scheduleItem);
+    
+    //convert array into string and store in local storage
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+
+});
 
